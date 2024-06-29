50218483d8e906cfe352255ee540228948042b18
DucAnh <anhnguyen2k373@gmail.com>
2024-06-29 08:41:48 +0700

    signup and signin theme

--- project/settings.py
+++ project/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 load_dotenv()
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": os.getenv('DB_ENGINE'),
-#         "NAME": os.getenv('DB_NAME'),
-#         "USER": os.getenv('DB_USER'),
-#         "PASSWORD": os.getenv('DB_PASSWORD'),
-#         "HOST": os.getenv('DB_HOST'),
-#         "PORT": os.getenv('DB_PORT'),
-#     }
-# }   
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.getenv('DB_ENGINE'),
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "HOST": os.getenv('DB_HOST'),
+        "PORT": os.getenv('DB_PORT'),
     }
-}
+}   
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
