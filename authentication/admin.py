3abd2f9ff5db33b6679a2fc4ee074d3f68591661
DucAnh <anhnguyen2k373@gmail.com>
2024-07-03 22:31:06 +0700

    Dashboard 8

--- authentication/admin.py
+++ authentication/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from unfold.admin import ModelAdmin
 
-# Custom admin classes
+# Định nghĩa các lớp quản trị tùy chỉnh
 class CustomUserAdmin(ModelAdmin):
     actions_list = ['add_to_students_group']
-    search_fields = ('username', 'email')  # Ensure these fields are indexed and searchable
+    search_fields = ('username', 'email')
     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
     fieldsets = (
@@ -14,14 +14,16 @@ class CustomUserAdmin(ModelAdmin):
         ('Important dates', {'fields': ('last_login',)}),
     )
 
-
 class CustomGroupAdmin(ModelAdmin):
     pass
 
-# Unregister the default admin
-admin.site.unregister(User)
-admin.site.unregister(Group)
+# Hủy đăng ký admin mặc định
+try:
+    admin.site.unregister(User)
+    admin.site.unregister(Group)
+except admin.sites.NotRegistered:
+    pass  # Bỏ qua nếu đã bị hủy đăng ký
 
-# Register custom admin
+# Đăng ký admin tùy chỉnh
 admin.site.register(User, CustomUserAdmin)
 admin.site.register(Group, CustomGroupAdmin)
