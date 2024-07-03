a36bcddd332453b13fcddf4cdecfb24b12b81782
DucAnh <anhnguyen2k373@gmail.com>
2024-07-03 20:35:52 +0700

    Dashboard 7

--- authentication/admin.py
+++ authentication/admin.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User, Group
 from unfold.admin import ModelAdmin
 
 
+
 # Định nghĩa các lớp quản trị tùy chỉnh
 class CustomUserAdmin(ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
@@ -25,6 +26,3 @@ admin.site.unregister(Group)  # Hủy đăng ký mặc định
 
 admin.site.register(User, CustomUserAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
 admin.site.register(Group, CustomGroupAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
-
-
-
