ea3c16c58f05cd2ec16827d44bfe9eb9afffcd6b
DucAnh <anhnguyen2k373@gmail.com>
2024-07-03 12:01:10 +0700

    sso 7

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from unfold.admin import ModelAdmin
-from django_google_sso.models import GoogleSSOUser
 
 # Định nghĩa các lớp quản trị tùy chỉnh
 class CustomUserAdmin(ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
+    list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
     fieldsets = (
         (None, {'fields': ('username', 'email', 'date_joined')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
@@ -15,15 +15,12 @@ class CustomUserAdmin(ModelAdmin):
 class CustomGroupAdmin(ModelAdmin):
     pass
 
-class CustomGoogleSSOUserAdmin(ModelAdmin):
-    list_display = ('id', 'google_id',)
+
 
 
 # Đăng ký các lớp quản trị với admin site
 admin.site.unregister(User)  # Hủy đăng ký mặc định
 admin.site.unregister(Group)  # Hủy đăng ký mặc định
-admin.site.unregister(GoogleSSOUser)  # Hủy đăng ký mặc định
 
 admin.site.register(User, CustomUserAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
 admin.site.register(Group, CustomGroupAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
-admin.site.register(GoogleSSOUser, CustomGoogleSSOUserAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
