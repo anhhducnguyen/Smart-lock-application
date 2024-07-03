7b8c4111caff71a6e524a380145626280828ebb8
DucAnh <anhnguyen2k373@gmail.com>
2024-07-03 16:22:46 +0700

    Dashboard 7

--- project/settings.py
+++ project/settings.py
@@ -189,7 +189,6 @@ from django.utils.translation import gettext_lazy as _
 UNFOLD = {
     "SITE_TITLE": None,
     "SITE_HEADER": "FLOWER SHOP",
-    "DASHBOARD_CALLBACK": "sample_app.dashboard_callback",
     "EXTENSIONS": {
         "modeltranslation": {
             "flags": {
