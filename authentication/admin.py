b92290be779472f984820b8ef5d4ddae6dc9e032
DucAnh <anhnguyen2k373@gmail.com>
2024-07-03 14:07:50 +0700

    sso 18

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from unfold.admin import ModelAdmin
+from django_google_sso.models import GoogleSSOUser
+
+
 
 # Định nghĩa các lớp quản trị tùy chỉnh
 class CustomUserAdmin(ModelAdmin):
@@ -17,10 +20,14 @@ class CustomGroupAdmin(ModelAdmin):
     pass
 
 
+class CustomGoogleSSOUserAdmin(ModelAdmin):
+    pass
 
 # Đăng ký các lớp quản trị với admin site
 admin.site.unregister(User)  # Hủy đăng ký mặc định
 admin.site.unregister(Group)  # Hủy đăng ký mặc định
+admin.site.unregister(GoogleSSOUser)
 
 admin.site.register(User, CustomUserAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
 admin.site.register(Group, CustomGroupAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
+admin.site.register(GoogleSSOUser, CustomGoogleSSOUserAdmin)
