513ff6446cfd294a7d5744184c8f652c0a906ad8
DucAnh <anhnguyen2k373@gmail.com>
2024-07-11 22:03:50 +0700

    models

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,75 +1,33 @@
-# from django.contrib import admin
-# from django.contrib.auth.models import User, Group
-# from unfold.admin import ModelAdmin
-
-
-
-# # Định nghĩa các lớp quản trị tùy chỉnh
-# class CustomUserAdmin(ModelAdmin):
-#     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
-#     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
-#     search_fields = ('username', 'email')
-#     fieldsets = (
-#         (None, {'fields': ('username', 'email', 'date_joined')}),
-#         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-#         ('Important dates', {'fields': ('last_login',)}),
-#     )
-
-
-
-# class CustomGroupAdmin(ModelAdmin):
-#     search_fields = ('name')
-#     # pass
-
-
-# # Đăng ký các lớp quản trị với admin site
-# admin.site.unregister(User)  # Hủy đăng ký mặc định
-# admin.site.unregister(Group)  # Hủy đăng ký mặc định
-
-
-# admin.site.register(User, CustomUserAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
-# admin.site.register(Group, CustomGroupAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
-
-
-# admin.py
-
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import User, Group
 from unfold.admin import ModelAdmin
-from django.contrib.auth.models import User
 
-@admin.register(User)
+
+
+# Định nghĩa các lớp quản trị tùy chỉnh
 class CustomUserAdmin(ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email')
     fieldsets = (
-        (
-            None,
-            {
-                "fields": ('username', 'email', 'date_joined'),
-            },
-        ),
-        (
-            _("Permissions"),
-            {
-                "classes": ["tab"],
-                "fields": ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
-            },
-        ),
-        (
-            _("Important dates"),
-            {
-                "classes": ["tab"],
-                "fields": ('last_login',),
-            },
-        ),
+        (None, {'fields': ('username', 'email', 'date_joined')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
     )
 
-# # Đăng ký các lớp quản trị với admin site
+
+
+class CustomGroupAdmin(ModelAdmin):
+    search_fields = ('name')
+    # pass
+
+
+# Đăng ký các lớp quản trị với admin site
 admin.site.unregister(User)  # Hủy đăng ký mặc định
-# admin.site.unregister(Group)  # Hủy đăng ký mặc định
+admin.site.unregister(Group)  # Hủy đăng ký mặc định
 
 
 admin.site.register(User, CustomUserAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
-# admin.site.register(Group, CustomGroupAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
+admin.site.register(Group, CustomGroupAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
+
+
