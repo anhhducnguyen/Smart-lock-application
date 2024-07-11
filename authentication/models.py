bbb60096ae273e610686716734a2e59725b34dae
DucAnh <anhnguyen2k373@gmail.com>
2024-07-11 21:45:15 +0700

    models

--- authentication/models.py
+++ authentication/models.py
@@ -1,19 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-# models.py
-
-from django.contrib.admin import register
-from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-from unfold.admin import ModelAdmin
-from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
-
-
-# @register(User)
-# class UserAdmin(BaseUserAdmin, ModelAdmin):
-#     form = UserChangeForm
-#     add_form = UserCreationForm
-#     change_password_form = AdminPasswordChangeForm
