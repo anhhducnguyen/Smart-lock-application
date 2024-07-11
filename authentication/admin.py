14e346c3ba8eba46aad70c0d7497cc12969536b6
DucAnh <anhnguyen2k373@gmail.com>
2024-07-11 21:53:58 +0700

    models

--- authentication/admin.py
+++ authentication/admin.py
@@ -5,43 +5,43 @@ from unfold.admin import ModelAdmin
 
 
 # Định nghĩa các lớp quản trị tùy chỉnh
-# class CustomUserAdmin(ModelAdmin):
-#     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
-#     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
-#     search_fields = ('username', 'email')
-#     fieldsets = (
-#         (None, {'fields': ('username', 'email', 'date_joined')}),
-#         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-#         ('Important dates', {'fields': ('last_login',)}),
-#     )
-
-@admin.register(User)
 class CustomUserAdmin(ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email')
     fieldsets = (
-        (
-            None,
-            {
-                "fields": ('username', 'email', 'date_joined'),
-            },
-        ),
-        (
-            _("Permissions"),
-            {
-                "classes": ["tab"],
-                "fields": ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
-            },
-        ),
-        (
-            _("Important dates"),
-            {
-                "classes": ["tab"],
-                "fields": ('last_login',),
-            },
-        ),
+        (None, {'fields': ('username', 'email', 'date_joined')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login',)}),
     )
+
+# @admin.register(User)
+# class CustomUserAdmin(ModelAdmin):
+#     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
+#     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
+#     search_fields = ('username', 'email')
+#     fieldsets = (
+#         (
+#             None,
+#             {
+#                 "fields": ('username', 'email', 'date_joined'),
+#             },
+#         ),
+#         (
+#             _("Permissions"),
+#             {
+#                 "classes": ["tab"],
+#                 "fields": ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+#             },
+#         ),
+#         (
+#             _("Important dates"),
+#             {
+#                 "classes": ["tab"],
+#                 "fields": ('last_login',),
+#             },
+#         ),
+#     )
 
 
 class CustomGroupAdmin(ModelAdmin):
