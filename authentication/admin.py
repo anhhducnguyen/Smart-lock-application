c67238a0c75cbcfbf44d54b31f4853451433ffa7
DucAnh <anhnguyen2k373@gmail.com>
2024-07-11 21:52:43 +0700

    models

--- authentication/admin.py
+++ authentication/admin.py
@@ -5,14 +5,42 @@ from unfold.admin import ModelAdmin
 
 
 # Định nghĩa các lớp quản trị tùy chỉnh
+# class CustomUserAdmin(ModelAdmin):
+#     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
+#     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
+#     search_fields = ('username', 'email')
+#     fieldsets = (
+#         (None, {'fields': ('username', 'email', 'date_joined')}),
+#         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+#         ('Important dates', {'fields': ('last_login',)}),
+#     )
+
+@admin.register(User)
 class CustomUserAdmin(ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email')
     fieldsets = (
-        (None, {'fields': ('username', 'email', 'date_joined')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login',)}),
+        (
+            None,
+            {
+                "fields": ('username', 'email', 'date_joined'),
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "classes": ["tab"],
+                "fields": ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+            },
+        ),
+        (
+            _("Important dates"),
+            {
+                "classes": ["tab"],
+                "fields": ('last_login',),
+            },
+        ),
     )
 
 
@@ -30,45 +58,3 @@ admin.site.register(User, CustomUserAdmin)  # Đăng ký với lớp quản tr�
 admin.site.register(Group, CustomGroupAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
 
 
-# admin.py
-
-from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
-from unfold.admin import ModelAdmin
-
-from .models import MyModel
-
-
-@admin.register(MyModel)
-class MyModelAdmin(ModelAdmin):
-    fieldsets = (
-        (
-            None,
-            {
-                "fields": [
-                    "field_1",
-                    "field_2",
-                ],
-            },
-        ),
-        (
-            _("Tab 1"),
-            {
-                "classes": ["tab"],
-                "fields": [
-                    "field_3",
-                    "field_4",
-                ],
-            },
-        ),
-        (
-            _("Tab 2"),
-            {
-                "classes": ["tab"],
-                "fields": [
-                    "field_5",
-                    "field_6",
-                ],
-            },
-        ),
-    )
