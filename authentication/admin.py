0fbbac7133757aed4c9889faec11469cb35dc2e5
DucAnh <anhnguyen2k373@gmail.com>
2024-07-12 23:26:01 +0700

    test

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.contrib.auth.models import User, Group
 from unfold.admin import ModelAdmin
+from unfold.admin import ModelAdmin, Tab
 
 
 
 # Định nghĩa các lớp quản trị tùy chỉnh
 class CustomUserAdmin(ModelAdmin):
+    tabs = [
+        Tab(label='Nhóm người dùng', url='/admin/auth/group/'),
+        Tab(label='Người dùng không hoạt động', url='/admin/auth/user/?is_active__exact=0'),
+    ]
     list_display = ('username', 'email', 'date_joined', 'is_active', 'is_staff', 'is_superuser')
     list_filter = ('username', 'email', 'is_active', 'is_staff', 'is_superuser')
     search_fields = ('username', 'email')
