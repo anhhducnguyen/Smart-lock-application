d588ee9651252559542f066beff2896dfd409988
DucAnh <anhnguyen2k373@gmail.com>
2024-07-16 22:07:04 +0700

    test

--- project/settings.py
+++ project/settings.py
@@ -199,41 +199,27 @@ UNFOLD = {
     },
     "SITE_TITLE": None,
     "SITE_HEADER": "FLOWER SHOP",
-    "COLORS": {
-        "primary": {
-            "50": "250 245 255",
-            "100": "243 232 255",
-            "200": "233 213 255",
-            "300": "216 180 254",
-            "400": "192 132 252",
-            "500": "168 85 247",
-            "600": "147 51 234",
-            "700": "126 34 206",
-            "800": "107 33 168",
-            "900": "88 28 135",
-            "950": "59 7 100",
+    "TABS": 
+    [
+        {
+            "models": ["project.user"],
+            "items": [
+                {
+                    "title": _("Drivers"),
+                    "icon": "sports_motorsports",
+                    "link": reverse_lazy("admin:auth_group_changelist"),
+                },
+                {
+                    "title": _("Constructors"),
+                    "icon": "precision_manufacturing",
+                    "link": reverse_lazy("admin:auth_group_changelist"),
+                },
+            ],
         },
-    },
+    ],
     "SIDEBAR": {
         "show_search": True,  # Search in applications and models names
         "show_all_applications": True,  # Dropdown with all applications and models
-        "TABS": [
-            {
-                "models": ["project.user"],
-                "items": [
-                    {
-                        "title": _("Drivers"),
-                        "icon": "sports_motorsports",
-                        "link": reverse_lazy("admin:auth_group_changelist"),
-                    },
-                    {
-                        "title": _("Constructors"),
-                        "icon": "precision_manufacturing",
-                        "link": reverse_lazy("admin:auth_group_changelist"),
-                    },
-                ],
-            },
-        ],
         "navigation": [
             {
                 "title": _("Navigation"),
