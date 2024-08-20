f2a16e9928588e04c95b04f0bcb9ce0a4efd6d1b
DucAnh <anhnguyen2k373@gmail.com>
2024-08-20 22:41:01 +0700

    edit

--- project/urls.py
+++ project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from project import views
+
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
 # ]
@@ -25,4 +27,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authentication.urls')),
+    path('analytics', views.analytics, name='analytics'),
 ]
--- project/views.py
+++ project/views.py
@@ -31,7 +31,7 @@ def dashboard_callback(request, context):
         {
             "navigation": [
                 {"title": _("Dashboard"), "link": "/", "active": True},
-                {"title": _("Analytics"), "link": "#"},
+                {"title": _("Analytics"), "link": "analytics"},
                 {"title": _("Settings"), "link": "#"},
             ],
             "filters": [
@@ -151,6 +151,96 @@ def dashboard_callback(request, context):
                             ],
                         }
                     ),
+                },
+            ],
+        },
+    )
+
+    return context
+
+
+def analytics(request, context):
+    WEEKDAYS = [
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+        "Sun",
+    ]
+
+    positive = [[1, random.randrange(8, 28)] for i in range(1, 28)]
+    average = [r[1] - random.randint(3, 5) for r in positive]
+   
+    context.update(
+        {
+            "navigation": [
+                {"title": _("Dashboard"), "link": "/", "active": True},
+                {"title": _("Analytics"), "link": "#"},
+                {"title": _("Settings"), "link": "#"},
+            ],
+            "filters": [
+                {"title": _("All"), "link": "#", "active": True},
+                {
+                    "title": _("New"),
+                    "link": "#",
+                },
+            ],
+            "kpi": [
+                {
+                    "title": "Product A Performance",
+                    "metric": "$1,234.56",
+                    "footer": mark_safe(
+                        '<strong class="text-green-600 font-medium">+3.14%</strong>&nbsp;progress from last week'
+                    ),
+                    "chart": json.dumps(
+                        {
+                            "labels": [WEEKDAYS[day % 7] for day in range(1, 28)],
+                            "datasets": [{"data": average, "borderColor": "#9333ea"}],
+                        }
+                    ),
+                },
+                {
+                    "title": "Product B Performance",
+                    "metric": "$1,234.56",
+                    "footer": mark_safe(
+                        '<strong class="text-green-600 font-medium">+3.14%</strong>&nbsp;progress from last week'
+                    ),
+                },
+                {
+                    "title": "Product C Performance",
+                    "metric": "$1,234.56",
+                    "footer": mark_safe(
+                        '<strong class="text-green-600 font-medium">+3.14%</strong>&nbsp;progress from last week'
+                    ),
+                },
+            ],
+            "progress": [
+                {
+                    "title": "Social marketing e-book",
+                    "description": " $1,234.56",
+                    "value": random.randint(10, 90),
+                },
+                {
+                    "title": "Freelancing tasks",
+                    "description": " $1,234.56",
+                    "value": random.randint(10, 90),
+                },
+                {
+                    "title": "Development coaching",
+                    "description": " $1,234.56",
+                    "value": random.randint(10, 90),
+                },
+                {
+                    "title": "Product consulting",
+                    "description": " $1,234.56",
+                    "value": random.randint(10, 90),
+                },
+                {
+                    "title": "Other income",
+                    "description": " $1,234.56",
+                    "value": random.randint(10, 90),
                 },
             ],
         },
