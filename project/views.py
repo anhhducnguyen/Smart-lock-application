9c9dc3172190b097e0c4f2a734a72556e8a6fe75
DucAnh <anhnguyen2k373@gmail.com>
2024-08-20 22:42:37 +0700

    edit

--- project/views.py
+++ project/views.py
@@ -31,7 +31,7 @@ def dashboard_callback(request, context):
         {
             "navigation": [
                 {"title": _("Dashboard"), "link": "/", "active": True},
-                {"title": _("Analytics"), "link": "analytics"},
+                {"title": _("Analytics"), "link": "/analytics"},
                 {"title": _("Settings"), "link": "#"},
             ],
             "filters": [
