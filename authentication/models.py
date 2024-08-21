1d1c8054960ae238bb06d17f10b95fb9835c5d77
DucAnh <anhnguyen2k373@gmail.com>
2024-08-21 20:38:31 +0700

    models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class GoogleSSOUser(models.Model):
+    google_id = models.CharField(max_length=255, unique=True)
+    locale = models.CharField(max_length=10)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture_url = models.URLField(max_length=200, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
