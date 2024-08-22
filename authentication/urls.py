07ca401fc6f321605c82008959bdf1cdacea1b93
DucAnh <anhnguyen2k373@gmail.com>
2024-08-22 22:42:49 +0700

    settings

--- authentication/urls.py
+++ authentication/urls.py
@@ -16,7 +16,7 @@ urlpatterns = [
     path('forgotPassword', views.forgotPassword, name='forgotPassword'),
 
     # Quên mật khẩu
-    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset'),
     path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
