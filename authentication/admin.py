d7feb67ef574f5bc3b8b97f7558881d30c2d4bc2
DucAnh <anhnguyen2k373@gmail.com>
2024-08-22 07:16:22 +0700

    models

--- authentication/admin.py
+++ authentication/admin.py
@@ -75,8 +75,7 @@ class CustomGoogleSSOUserAdmin(ModelAdmin):
 
 admin.site.unregister(User)  # Hủy đăng ký mặc định
 admin.site.unregister(Group)  # Hủy đăng ký mặc định
-# admin.site.unregister(GoogleSSOUser)
-
+admin.site.unregister(GoogleSSOUser)
 
 admin.site.register(User, CustomUserAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
 admin.site.register(Group, CustomGroupAdmin)  # Đăng ký với lớp quản trị tùy chỉnh
