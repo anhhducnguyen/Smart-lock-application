b3180515d11677585b2828218b41b7dbb9181fd4
DucAnh <anhnguyen2k373@gmail.com>
2024-08-22 10:47:30 +0700

    settings

--- project/settings.py
+++ project/settings.py
@@ -202,7 +202,7 @@ UNFOLD = {
     },
     "DASHBOARD_CALLBACK": "project.views.dashboard_callback",
     "LOGIN": {
-        "image": lambda request: static("assets/images/log.svg"),
+        "image": lambda request: static("icons/undraw_programming_re_kg9v.svg"),
     },
     # "SITE_TITLE": None,
     "SITE_TITLE": _("Smart car park"),
