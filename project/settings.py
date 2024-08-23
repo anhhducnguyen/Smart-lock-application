e611b9237201e2f602ca14bcc3844d0903defae6
DucAnh <anhnguyen2k373@gmail.com>
2024-08-23 22:39:24 +0700

    readme

--- project/settings.py
+++ project/settings.py
@@ -312,4 +312,14 @@ UNFOLD = {
 DEFAULT_CONTENT_TYPE = 'text/html'
 
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'anhnguyen2k373@gmail.com'
+EMAIL_HOST_PASSWORD = 'apjahlrpflvncpfy'
+
+
+
+
 
