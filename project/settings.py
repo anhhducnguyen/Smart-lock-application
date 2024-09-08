0d35af2ef2bc57012e906892b1dbe2ab0ad6f5d6
DucAnh <anhnguyen2k373@gmail.com>
2024-09-08 23:00:12 +0700

    edit new theme admin

--- project/settings.py
+++ project/settings.py
@@ -241,7 +241,7 @@ UNFOLD = {
     },
     "DASHBOARD_CALLBACK": "project.views.dashboard_callback",
     "LOGIN": {
-        "image": lambda request: static("icons/undraw_hacker_mind_-6-y85.svg"),
+        "image": lambda request: static("icons/illustration.jpg"),
     },
     # "SITE_TITLE": None,
     "SITE_TITLE": _("Smart car park"),
