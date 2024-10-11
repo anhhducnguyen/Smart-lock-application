ac080d91481c8e83561f1d004ed49e74c8ed21c5
DucAnh <anhnguyen2k373@gmail.com>
2024-10-11 18:55:27 +0700

    edit view

--- authentication/views.py
+++ authentication/views.py
@@ -113,74 +113,6 @@ def capture_image(request):
 
     return JsonResponse({"message": "Lỗi khi lưu ảnh!"}, status=400)
 
-# def signup(request):
-#     if request.method == "POST":
-#         username = request.POST['username']
-#         fname = request.POST['fname']
-#         # lname = request.POST['lname']
-#         email = request.POST['email']
-#         pass1 = request.POST['pass1']
-#         pass2 = request.POST['pass2']
-        
-#         if User.objects.filter(username=username):
-#             messages.error(request, "Username already exist! Please try some other username.")
-#             return redirect('home')
-        
-#         if User.objects.filter(email=email).exists():
-#             messages.error(request, "Email Already Registered!!")
-#             return redirect('home')
-        
-#         if len(username)>20:
-#             messages.error(request, "Username must be under 20 charcters!!")
-#             return redirect('home')
-        
-#         if pass1 != pass2:
-#             messages.error(request, "Passwords didn't matched!!")
-#             return redirect('home')
-        
-#         if not username.isalnum():
-#             messages.error(request, "Username must be Alpha-Numeric!!")
-#             return redirect('home')
-        
-#         myuser = User.objects.create_user(username, email, pass1)
-#         myuser.first_name = fname
-#         # myuser.last_name = lname
-#         # myuser.is_active = False
-#         myuser.is_active = False
-#         myuser.save()
-#         messages.success(request, "Your Account has been created succesfully!! Please check your email to confirm your email address in order to activate your account.")
-        
-#         # Welcome Email
-#         subject = "Welcome to GFG- Django Login!!"
-#         message = "Hello " + myuser.first_name + "!! \n" + "Welcome to GFG!! \nThank you for visiting our website\n. We have also sent you a confirmation email, please confirm your email address. \n\nThanking You\nAnubhav Madhav"        
-#         from_email = settings.EMAIL_HOST_USER
-#         to_list = [myuser.email]
-#         send_mail(subject, message, from_email, to_list, fail_silently=True)
-        
-#         # Email Address Confirmation Email
-#         current_site = get_current_site(request)
-#         email_subject = "Confirm your Email @ GFG - Django Login!!"
-#         message2 = render_to_string('email_confirmation.html',{
-            
-#             'name': myuser.first_name,
-#             'domain': current_site.domain,
-#             'uid': urlsafe_base64_encode(force_bytes(myuser.pk)),
-#             'token': generate_token.make_token(myuser)
-#         })
-#         email = EmailMessage(
-#         email_subject,
-#         message2,
-#         settings.EMAIL_HOST_USER,
-#         [myuser.email],
-#         )
-#         email.fail_silently = True
-#         email.send()
-        
-#         return redirect('signin')
-        
-        
-#     return render(request, "authentication/signup.html")
-
 def signup(request):
     if request.method == "POST":
         username = request.POST['username']
@@ -263,26 +195,6 @@ def activate(request,uidb64,token):
     else:
         return render(request,'activation_failed.html')
 
-
-# def signin(request):
-#     if request.method == 'POST':
-#         username = request.POST['username']
-#         pass1 = request.POST['pass1']
-        
-#         user = authenticate(username=username, password=pass1)
-        
-#         if user is not None:
-#             login(request, user)
-#             fname = user.first_name
-#             # messages.success(request, "Logged In Sucessfully!!")
-#             return render(request, "authentication/index.html",{"fname":fname})
-#             # return redirect('home')
-#         else:
-#             messages.error(request, "Bad Credentials!!")
-#             return redirect('home')
-    
-#     return render(request, "authentication/signin.html")
-
 def signin(request):
     if request.method == 'POST':
         username = request.POST['username']
@@ -306,3 +218,6 @@ def signout(request):
     logout(request)
     messages.success(request, "Logged Out Successfully!!")
     return redirect('home')
+
+
+
